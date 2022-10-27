fe470ced20088850ede5fc767fea2034c4769f50
LinoyHi <linoyhib14@walla.co.il>
2022-10-27 13:01:11 +0300

    userDTO made

--- web-server/src/users/dto/create-user.dto.ts
+++ web-server/src/users/dto/create-user.dto.ts
@@ -1,1 +1,29 @@
-export class CreateUserDto {}
+import {
+    IsEmail,
+    IsNotEmpty,
+  } from 'class-validator';
+  
+export class CreateUserDto {
+    @IsNotEmpty()
+    name: string;
+
+    @IsNotEmpty()
+    password: string;
+
+    @IsNotEmpty()
+    firstName: string;
+
+    @IsNotEmpty()
+    lastName: string
+
+    @IsEmail()
+    email: string;
+    
+    address: string;
+
+    city: string;
+
+    country: string;
+
+    phone: string;
+}
