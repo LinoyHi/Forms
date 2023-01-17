7d2979a3214a15c2a2c8b200988d4f263987072b
LinoyHi <linoyhib14@walla.co.il>
2023-01-17 17:13:24 +0200

    changes in the user slice now updated in the home comp

--- website/src/components/Home/home.js
+++ website/src/components/Home/home.js
@@ -6,11 +6,12 @@ export function Home() {
     const userData = useSelector((state)=>state.user);
     const dispatch = useDispatch();
     const navigate = useNavigate()
+    console.log(userData)
 
     return (
         <>
-            <h1>welcome {userData?.firstName || <span className="App-link" onClick={() => navigate('/')}>log in first</span>}</h1>
-            {userData.firstName? <button onClick={()=>dispatch(disconnect())}>log out</button>: 
+            <h1>welcome {userData?.user?.firstName || <span className="App-link" onClick={() => navigate('/')}>log in first</span>}</h1>
+            {userData?.user?.firstName? <button onClick={()=>dispatch(disconnect())}>log out</button>: 
             <button onClick={()=>navigate('/signup')}>sign up</button>}
         </>
         
