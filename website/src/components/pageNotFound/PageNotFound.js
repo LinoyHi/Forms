d36798c65a226d3ac8d2899e3e4a191bd9832869
LinoyHi <linoyhib14@walla.co.il>
2023-01-19 17:08:07 +0200

    added light grey backgroung to any Links

--- website/src/components/pageNotFound/PageNotFound.js
+++ website/src/components/pageNotFound/PageNotFound.js
@@ -1,23 +1,27 @@
 import { Link, useParams } from "react-router-dom";
 
-export default function PageNotFound() {   
+export default function PageNotFound() {
     const { catchAll } = useParams()
 
     return (
         <div className="text-center">
             <h1>No Page is called {catchAll}</h1>
-            <span>go to: 
-                <br/>
-                <Link to={'/home'}>
-                    HomePage as guest
-                </Link> OR 
-                <Link to={'/signup'}>
-                    Sign up
-                </Link> 
-                <br/> already have an acount? 
-                <Link to={'/'}>
-                    Log In
-                </Link>
+            <span>go to:
+                <br />
+                <div className="p-1 text-bg-light">
+                    <span> already have an acount? </span>
+                    <Link to={'/'}>
+                        Log In
+                    </Link>
+                    <br />
+                    <Link to={'/home'}>
+                        Home as a guest
+                    </Link>
+                    <span> OR </span>
+                    <Link to={'/signup'}>
+                        Sign up
+                    </Link>
+                </div>
             </span>
         </div>
     )
