760940873c9b3a6b9b3386eb38ed03a64c70d261
LinoyHi <linoyhib14@walla.co.il>
2023-06-14 16:08:20 +0300

    using asyncThunk for login

--- website/src/components/forms/logIn.js
+++ website/src/components/forms/logIn.js
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { validate } from "../../common/validation";
-import { getLoginFormData, checkUser, fogotPasswordMail } from "../../DAL/api";
+import { getLoginFormData, fogotPasswordMail } from "../../DAL/api";
 import Formcomp from "./formComp";
-import { useDispatch } from "react-redux";
-import { changeUser, addChangePasswordExpiration } from "../../features/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { changeUser, addChangePasswordExpiration, connectUser } from "../../features/user/userSlice";
 
 export default function LogIn() {
+    const { user } = useSelector((state)=>state.user);
     const [data, setdata] = useState(null)
     const dispatch = useDispatch();
+
 
     useEffect(() => {
         async function getLoginData() {
@@ -18,42 +20,37 @@ export default function LogIn() {
     }, [])
     const navigate = useNavigate()
 
-    async function setuser(allset) {
-        const user = await checkUser(validate(allset.usernameOrEmail.value, { email: true }, "usernameOrEmail") ?
-            { username: allset.usernameOrEmail.value, password: allset.password.value }
-            : { email: allset.usernameOrEmail.value, password: allset.password.value }
-        )
-        if (user.ok === true) {
-            const userData = await user.json()
-            const date = new Date()
-            date.setDate(date.getDate() + 30)
-            dispatch(changeUser({name:userData.name,firstName:userData.firstName,manager:userData.manager,ConExpiration:date}))
-            navigate('/home')
-        }
-        else {
-            alert('user name or pasword is wrong')
-        }
+    function setuser(allset) {
+        dispatch(connectUser(allset))
+        user ? navigate('/home') : alert('password or username is incorrect')
     }
 
     async function forgot_password() {
-        const date = new Date();
-        date.setDate(date.getDate() + 7);
-        dispatch(addChangePasswordExpiration(JSON.stringify(date)))
-        const identifier = data.usernameOrEmail.value
-        let fp = undefined
-        if (identifier) {
-            validate(identifier, { email: true }, "usernameOrEmail") ?
-                dispatch(changeUser({ username: identifier }))
-                : dispatch(changeUser({ email: identifier }))
-            validate(identifier, { email: true }, "usernameOrEmail") ?
-                fp = await fogotPasswordMail({ username: identifier, expiredate: date }) :
-                fp = await fogotPasswordMail({ email: identifier, expiredate: date })
-            if (fp.ok) {
-                navigate('/authorize')
-                return
+        const identifierObj = data.usernameOrEmail
+        if(!identifierObj.eror){
+
+            const date = new Date();
+            date.setDate(date.getDate() + 7);
+            dispatch(addChangePasswordExpiration(JSON.stringify(date)))
+            const identifier = identifierObj.value
+            let fp = undefined
+            if (identifier) {
+                validate(identifier, { email: true }, "usernameOrEmail") ?
+                    dispatch(changeUser({ username: identifier }))
+                    : dispatch(changeUser({ email: identifier }))
+                validate(identifier, { email: true }, "usernameOrEmail") ?
+                    fp = await fogotPasswordMail({ username: identifier, expiredate: date }) :
+                    fp = await fogotPasswordMail({ email: identifier, expiredate: date })
+                if (fp.ok) {
+                    navigate('/authorize')
+                    return
+                }
             }
+            identifierObj.eror = 'please enter user name or email before clicking on forgot password'
         }
-        alert('please enter valid user name or your email first')
+        if(!identifierObj.eror.includes('valid')) {
+            identifierObj.eror+=', please enter a valid user name or email before clicking on forgot password'
+        }
     }
 
     return (
@@ -63,7 +60,7 @@ export default function LogIn() {
                     data={data} submitName='enter'></Formcomp> : <h1>please wait..</h1>}
             <div className="p-1 text-bg-light">
                 <Link onClick={forgot_password}>forgot password?</Link>
-                <br />
+                <br/>
                 <span><Link to={'/home'}>Enter as guest</Link> OR <Link to={'/signup'}>Sign up</Link></span>
             </div>
         </div>
--- website/src/features/user/userSlice.js
+++ website/src/features/user/userSlice.js
@@ -1,27 +1,48 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { validate } from '../../common/validation';
+import { checkUser } from '../../DAL/api';
 
 const initialState = {
-    user : null,
+    user: null,
     expiredPasswordChange: null,
 }
+
+export const connectUser = createAsyncThunk("user/connect", async (allset, thunkAPI) => {
+    const user = await checkUser(validate(allset.usernameOrEmail.value, { email: true }, "usernameOrEmail") ?
+        { username: allset.usernameOrEmail.value, password: allset.password.value }
+        : { email: allset.usernameOrEmail.value, password: allset.password.value }
+    )
+    if (user.ok === true) {
+        const userData = await user.json()
+        const date = new Date()
+        date.setDate(date.getDate() + 30)
+        return { name: userData.name, firstName: userData.firstName, manager: userData.manager, p: userData.password, ConExpiration: date }
+        //p is the permission to reconnect the user to the server in case the server falls
+    }
+})
 
 export const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
         changeUser: (state, action) => {
-            state.user? state.user = {...state.user, ...action.payload}:state.user = action.payload;
+            state.user ? state.user = { ...state.user, ...action.payload } : state.user = action.payload;
         },
         disconnect: (state) => {
             state.user = null
             fetch('http://localhost:4005/users/logout', { credentials: 'include' })
         },
-        addChangePasswordExpiration(state, date){
+        addChangePasswordExpiration(state, date) {
             state.expiredPasswordChange = date.payload
         }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(connectUser.fulfilled,(state,action)=>{
+            state.user = action.payload
+        })
     }
 })
 
-export const {changeUser, disconnect, addChangePasswordExpiration} = userSlice.actions;
+export const { changeUser, disconnect, addChangePasswordExpiration } = userSlice.actions;
 
 export default userSlice.reducer;
